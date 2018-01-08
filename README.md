a07a7ee4bcc82d9bc0081edaba9990a97407e35c
Lithane <plok314159265@163.com>
2018-01-08 15:54:38 +0800

    Add support for multiple option configurations (0.27)

--- README.md
+++ README.md
@@ -5,11 +5,25 @@ With this script, you can practice on Questionor without having your hands leavi
 the keyboard. The key map is showed below.
 
 ```
+3 or 4 options:
          [A]        [B]       [C]         [D]
     `   1   2   3 \ 4   5 \ 6   7   8 / 9   0   -   =   | BKSP
   TAB |  Q   W   E | R   T | Y   U | I   O   P   [   ]  | \
   CAPS \ A   S   D | F   G | H   J | K   L   ;   '      | ENTER
   SHIFT \ Z   X   C \ V   B \ N   M \ ,   .   /        / SHIFT
+
+2 options:
+               [A]                    [B]
+    `   1   2   3   4   5 \ 6   7   8   9   0   -   =   | BKSP
+  TAB |  Q   W   E   R   T | Y   U   I   O   P   [   ]  | \
+  CAPS \ A   S   D   F   G | H   J   K   L   ;   '      | ENTER
+  SHIFT \ Z   X   C   V   B \ N   M   ,   .   /        / SHIFT
+
+more than 4 options:
+      `   1   2   3   4   5   6   7   8   9   0   -   =   | BKSP
+  TAB |  Q   W   E   R   T   Y   U   I   O   P   [   ]  | \
+  CAPS \[a] [b] [c] [d]  G   H  [e] [f] [g] [h]  '      | ENTER
+  SHIFT \ Z   X   C   V   B   N   M   ,   .   /        / SHIFT
 
   Above all that, special functions are binded to following keys:
   [Check/Next]:   ENTER, SPACE
@@ -27,6 +41,21 @@ Or you can directly copy the .js script into a new Tampermonkey script.
 ## Licensing
 
 This script is created by Lynz Rand, and licensed under GPLv3 License.
+
+## Changelog
+
+### 0.27
+
+- Add support for multiple option configurations
+
+### 0.25
+
+- Fixed some bug;
+- improved Readme
+
+### 0.1
+
+- Initial release
 
 [Q]: http://questionor.cn
 [G]: https://greasyfork.org/scripts/36731-questionor-keyboard-listener
--- questionorKeyboardListener.user.js
+++ questionorKeyboardListener.user.js
@@ -13,7 +13,7 @@ This script divides the keyboard into four main sections,
 which controls the four options:
 
          [A]        [B]       [C]         [D]
-    `   1   2   3 \ 4   5 \ 6   7   8 / 9   0   -   =   | BKSP
+    `   1   2   3 \ 4   5 \ -1   -2   -3 / 9   0   -   =   | BKSP
   TAB |  Q   W   E \ R   T \ Y   U | I   O   P   [   ]  | \
   CAPS \ A   S   D \ F   G \ H   J \ K   L   ;   '      | ENTER
   SHIFT \ Z   X   C \ V   B \ N   M | ,   .   /        / SHIFT
@@ -30,52 +30,129 @@ which controls the four options:
 (function () {
   'use strict';
   var unchecked = 1;
-  // 1 to 4 are for selecting options
-  // 6 is auto-check 
-  // 7 is force skip
-  // 8 is clear all
-  var a = new Array(
-    0, 0, 0, 0, 0, 0, 0, 0,
-    8, 7, 0, 0, 8, 6, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 8, 0, 0, 0, 0,
-    6, 0, 7, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 8, 0, // 47
-    // numbers
-    1, 1, 1, 2, 2,
-    3, 3, 3, 4, 4, // 57
+  // 1 to -3 are for selecting options
+  // -1 is auto-check 
+  // -2 is force skip
+  // -3 is clear all
+  var a = new Array(new Array(
+      // 1 ops
+      0, 0, 0, 0, 0, 0, 0, 0, -2, -1, 0, 0, -2, -1, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, -2, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, -2, 0, // 2-1
+      // numbers
+      1, 1, 1, 1, 1,
+      2, 2, 2, 2, 2, // 5-1
 
-    0, 0, 0, 0, 0, 0, 0,
-    // letters
-    1, 2, 1, 1, 1, 2, 2, 3,
-    4, 3, 4, 4, 4, 3, 4, 4,
-    1, 2, 1, 2, 3, 2, 1, 1,
-    3, 1,
-    0, 0, 0, 0, 0,
-    // keypad
-    8, 1, 2, 3, 4,
-    0, 0, 0, 0, 0,
-    7, 6, 0, 7, 7, 6, 7,
-    // Fn
-    0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0,
-    // symbols
-    0, 0,
-    0, 4, 4, 4, 8, 0, 0, 0
-  );
-  a[251] = 4; // too far away.
+      0, 0, 0, 0, 0, 0, 0,
+      // letters
+      1, 1, 1, 1, 1, 1, 1, 2,
+      2, 2, 2, 2, 2, 2, 2, 2,
+      1, 1, 1, 1, 2, 1, 1, 1,
+      2, 1,
+      0, 0, 0, 0, 0,
+      // keypad
+      -2, 1, 1, 2, 2,
+      0, 0, 0, 0, 0, -1, -1, 0, -1, -1, -1, -1,
+      // Fn
+      0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0,
+      // symbols
+      0, 0,
+      0, 2, 2, 2, -2, 0, 0, 0
+    ),
+    new Array(
+      0, 0, 0, 0, 0, 0, 0, 0, -3, -2, 0, 0, -3, -1, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, -3, 0, 0, 0, 0, -1, 0, -2, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, -3, 0, // 4-2
+      // numbers
+      1, 1, 1, 2, 2,
+      3, 3, 3, 4, 4, // 5-2
+
+      0, 0, 0, 0, 0, 0, 0,
+      // letters
+      1, 2, 1, 1, 1, 2, 2, 3,
+      4, 3, 4, 4, 4, 3, 4, 4,
+      1, 2, 1, 2, 3, 2, 1, 1,
+      3, 1,
+      0, 0, 0, 0, 0,
+      // keypad
+      -3, 1, 2, 3, 4,
+      0, 0, 0, 0, 0, -2, -1, 0, -2, -2, -1, -2,
+      // Fn
+      0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0,
+      // symbols
+      0, 0,
+      0, 4, 4, 4, -3, 0, 0, 0
+    ),
+    new Array(
+      0, 0, 0, 0, 0, 0, 0, 0, -3, -2, 0, 0, -3, -1, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, -3, 0, 0, 0, 0, -1, 0, -2, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, -3, 0, // 4-2
+      // numbers
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 5-2
+
+      0, 0, 0, 0, 0, 0, 0,
+      // letters
+      1, 0, 0, 3, 0, 4, 0, 0,
+      0, 5, 6, 7, 0, 0, 0, 0,
+      0, 0, 2, 0, 0, 0, 0, 0,
+      0, 0,
+      0, 0, 0, 0, 0,
+      // keypad
+      -3, 1, 2, 3, 4,
+      5, 6, 7, 8, 0, -2, -1, 0, -2, -2, -1, -2,
+      // Fn
+      0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0,
+      // symbols
+      0, 0,
+      0, 0, 0, 0, -3, 0, 0, 0
+    ));
+  a[0][251] = 2; // too far away.
+  a[1][254] = 4; // too far away.
+  a[2][186] = 8; // too far away.
   $(document).keydown(function (event) {
     var b = event.keyCode;
-    var c = a[b];
+    var l = $("label input").length;
+    var c;
+    switch (l) {
+      case 2:
+        c = a[0][b];
+        break;
+      case 3:
+      case 4:
+        c = a[1][b];
+        break;
+      case 5:
+      case 6:
+      case 7:
+      case 8:
+        c = a[2][b];
+        break;
+      default:
+        c = a[2][b];
+        break;
+    }
     // console.log("KeyCode", b, "->", c);
     if (c !== undefined && c !== 0) {
-      if (c <= 4 && c > 0){
+      if (c > 0) {
         // clicks option
-        $("label input")[c - 1].click()
-      }
-      else if (c == 6) {
+        try {
+          $("label input")[c - 1].click();
+        } catch (e) {
+          console.log(e);
+        }
+      } else if (c == -1) {
         // auto check & skip
         if (unchecked) {
           unchecked = 0;
@@ -84,11 +161,11 @@ which controls the four options:
           unchecked = 1;
           $(".qs-practice-btn-next").click();
         }
-      } else if (c == 7) {
+      } else if (c == -2) {
         // skip
         unchecked = 1;
         $(".qs-practice-btn-next").click();
-      } else if (c == 8) {
+      } else if (c == -3) {
         // clear all
         var options = $("label input").each(function (i, e) {
           e.checked = false;
